2a1f32920b8ca831df88ded33263627286cc44d1
Michael Wright <michael@level.co>
2024-06-18 19:32:44 -0600

    Enable rust-tools for language support

--- config/languages/rust-lang.nix
+++ config/languages/rust-lang.nix
@@ -1,10 +1,18 @@
 { ... }:
 
 {
-  plugins.lsp.servers.rust-analyzer = {
-    enable = true;
-    installCargo = true;
-    installRustc = true;
+  plugins = {
+    # Rust Tools configuration (on nixvim)
+    #   https://github.com/nix-community/nixvim/blob/main/plugins/languages/rust/rust-tools.nix
+    rust-tools = {
+      enable = true;
+    };
+
+    lsp.servers.rust-analyzer = {
+      enable = true;
+      installCargo = true;
+      installRustc = true;
+    };
   };
 }
 
