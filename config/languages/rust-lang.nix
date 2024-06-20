dd16e3c3aa2262e1acc006d5e413baa19e2b05d8
Michael Wright <michael@level.co>
2024-06-19 20:46:06 -0600

    Added rust-tools back for now

--- config/languages/rust-lang.nix
+++ config/languages/rust-lang.nix
@@ -7,6 +7,9 @@ in
   plugins = {
 
     #https://github.com/nix-community/nixvim/blob/main/plugins/languages/rust/rust-tools.nix
+    rust-tools = {
+      enable = true;
+    };
 
     lsp.servers.rust-analyzer = {
       enable = true;
