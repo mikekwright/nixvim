ba19a0f263748878a47265fd6b8a597a3e3c54b7
Michael Wright <michael@level.co>
2024-06-27 00:25:48 -0600

    Added a number of missing language servers

--- config/languages/default.nix
+++ config/languages/default.nix
@@ -8,11 +8,74 @@
   ];
 
   plugins = {
+    # Trying to use these as the defaults for nix
+    #   https://github.com/nix-community/nixvim/blob/main/plugins/lsp/language-servers/default.nix
     lsp = {
       enable = true;
       servers = {
+        # Lua support
         lua-ls.enable = true;
+
+        ## Web ones
+        # Typescript support
         tsserver.enable = true;
+        # Tailwind CSS support
+        tailwindcss.enable = true;
+        # HTML support
+        html.enable = true;
+        # Elm support
+        elmls.enable = true;
+        # CSS support
+        cssls.enable = true;
+        # Json support
+        jsonls.enable = true;
+
+        # Java support
+        java-language-server.enable = true;
+        # Scala support
+        metals.enable = true;
+
+
+        # Perl support
+        perlpls.enable = true;
+
+        # C# support
+        omnisharp.enable = true;
+
+
+        # Fotran
+        fortls.enable = true;
+        # Prolog support
+        prolog-ls.enable = true;
+
+        # golang support
+        gopls.enable = true;
+        golangci-lint-ls.enable = true;
+
+        # Kotlin support
+        kotlin-language-server.enable = true;
+
+        # Elixir support
+        lexical.enable = true;
+        elixirls.enable = true;
+
+        # nix
+        nil-ls.enable = true;
+        nixd.enable = true;
+
+        # Ruby
+        solargraph.enable = true;
+
+        # Zig support
+        zls.enable = true;
+
+        # SQL support
+        sqls.enable = true;
+
+        # Terraform support
+        terraformls.enable = true;
+        # Helm
+        helm-ls.enable = true;
       };
     };
 
--- config/languages/python-lang.nix
+++ config/languages/python-lang.nix
@@ -4,18 +4,25 @@
   # Review the list of available options for this plugin from nixvim
   # documentation: https://github.com/nix-community/nixvim/blob/main/plugins/lsp/language-servers/pylsp.nix
 
-  plugins.lsp.servers.pylsp.settings = {
-    enable = true;
+  plugins.lsp.servers = {
 
-    plugins = {
-      jedi_completion = {
-        enable = true;
-      };
+    pylyzer.enable = true;
+    # This is a rust language server for python
+    ruff.enable = true;
 
-      flake8 = {
-        enable = true;
-        config = {
-          ignore = "E501";
+    pylsp.settings = {
+      enable = true;
+
+      plugins = {
+        jedi_completion = {
+          enable = true;
+        };
+
+        flake8 = {
+          enable = true;
+          config = {
+            ignore = "E501";
+          };
         };
       };
     };
