f76e1e5d48ddabf580acd186b875b7095f519c0f
Michael Wright <michael@level.co>
2024-09-04 14:24:28 -0600

    Starting to take more shape, we are getting there

--- lib/default.nix
+++ lib/default.nix
@@ -11,6 +11,7 @@ let
 
       baseModule = {
         vimPackages = m.vimPackages or [];
+        vimOptPackages = m.vimOptPackages or [];
         lua = m.lua or "";
         packages = m.packages or [];
       };
@@ -19,20 +20,24 @@ let
 in {
   makeModule = m: 
     let
+      # The base module from flake is not a common module definition, this kicks off the
+      #   process of building and loading all the modules in the system.
       fullModule = mergeModule (m.module m.extraSpecialArgs) m.extraSpecialArgs;
 
-      # This is where the key logic for setting up the package
-      #   should go.
-      luaText = if builtins.hasAttr "lua" fullModule
-        then fullModule.lua
-        else "";
-
+      # Module packages are any other 3rd party packages that are needed when running neovim
+      #    such as rust, python, etc (mostly just lsp servers)
       modulePackages = if builtins.hasAttr "packages" fullModule
         then fullModule.packages
         else [];
 
+      # This concats the lua configuration to a single file that is called as the config
+      #   for neovim on boot.
+      luaText = if builtins.hasAttr "lua" fullModule
+        then fullModule.lua
+        else "";
       luaFile = (pkgs.writeText "init.lua" luaText);
 
+      # The actual neovim package solution.
       neovimPackage = neovim-pkgs.neovim.override {
         configure = {
           #  This is trying to load as a vimscript file, not lua.  Need to
@@ -42,18 +47,18 @@ in {
           #  Install all the needed plugins at this point.
           packages.myVimPackage = {
             start = fullModule.vimPackages;
-            opt = [ ];
+            opt = fullModule.vimOptPackages;
           };
         };
       };
-    in neovimPackage;
-    # in (pkgs.writeShellApplication {
-    #   name = "nvim";
-    #   runtimeInputs = [ neovimPackage ] ++ modulePackages;
-    #   text = ''
-    #     echo "Hello, ${luaFile}!" 
-    #     ${neovimPackage}/bin/nvim -u ${luaFile} "$@"
-    #   '';
-    # });
+    #in neovimPackage;
+    in (pkgs.writeShellApplication {
+      name = "nvim";
+      runtimeInputs = [ neovimPackage ] ++ modulePackages;
+      text = ''
+        echo "Hello, ${luaFile}!" 
+        ${neovimPackage}/bin/nvim -u ${luaFile} "$@"
+      '';
+    });
 }
 
