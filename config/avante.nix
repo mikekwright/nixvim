70ca02516daff1a55214b642e5d33a52a446ea93
Michael Wright <michael@level.co>
2025-06-02 10:41:35 -0600

    Added the avante debug false step to avoid too many messages

--- config/avante.nix
+++ config/avante.nix
@@ -4,8 +4,12 @@
 #    on neovim instead.
 let
   avante_lua = /*lua*/ ''
+    -- The load for the `avante_lib` needs to be called first to avoid other errors
+    require('avante_lib').load()
     require("avante").setup({
+      debug = false,
       provider = "gemini",
+      -- auto_suggestions_provider = "copilot",
 
       -- provider = "openai",
       -- openai = {
@@ -15,6 +19,68 @@ let
       --   temperature = 0, -- adjust if needed
       --   max_tokens = 4096,
       --   -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+      -- },
+
+      -- behaviour = {
+      --   auto_suggestions = false, -- Experimental stage
+      --   auto_set_highlight_group = true,
+      --   auto_set_keymaps = true,
+      --   auto_apply_diff_after_generation = false,
+      --   support_paste_from_clipboard = false,
+      -- },
+      -- mappings = {
+      --   --- @class AvanteConflictMappings
+      --   diff = {
+      --     ours = "co",
+      --     theirs = "ct",
+      --     all_theirs = "ca",
+      --     both = "cb",
+      --     cursor = "cc",
+      --     next = "]x",
+      --     prev = "[x",
+      --   },
+      --   suggestion = {
+      --     accept = "<M-l>",
+      --     next = "<M-]>",
+      --     prev = "<M-[>",
+      --     dismiss = "<C-]>",
+      --   },
+      --   jump = {
+      --     next = "]]",
+      --     prev = "[[",
+      --   },
+      --   submit = {
+      --     normal = "<CR>",
+      --     insert = "<C-s>",
+      --   },
+      --   sidebar = {
+      --     switch_windows = "<Tab>",
+      --     reverse_switch_windows = "<S-Tab>",
+      --   },
+      -- },
+      -- hints = { enabled = true },
+      -- windows = {
+      --   ---@type "right" | "left" | "top" | "bottom"
+      --   position = "right", -- the position of the sidebar
+      --   wrap = true, -- similar to vim.o.wrap
+      --   width = 50, -- default % based on available width
+      --   sidebar_header = {
+      --     align = "center", -- left, center, right for title
+      --     rounded = true,
+      --   },
+      -- },
+      -- highlights = {
+      --   ---@type AvanteConflictHighlights
+      --   diff = {
+      --     current = "DiffText",
+      --     incoming = "DiffAdd",
+      --   },
+      -- },
+      -- --- @class AvanteConflictUserConfig
+      -- diff = {
+      --   autojump = true,
+      --   ---@type string | fun(): any
+      --   list_opener = "copen",
       -- },
     })
   '';
