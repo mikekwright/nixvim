84ac9108d1a004d35d0a347c863e57e65f0de77d
Michael Wright <michael@level.co>
2025-11-25 08:54:23 -0700

    Enable emojis in the editor with blink

--- options/lsp/default.nix
+++ options/lsp/default.nix
@@ -27,7 +27,30 @@
           end,
 
           sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
+            default = { 'lsp', 'path', 'snippets', 'buffer', 'emoji' },
+
+            providers = {
+              emoji = {
+                module = "blink-emoji",
+                name = "Emoji",
+                score_offset = 15, -- Tune by preference
+                opts = {
+                  insert = true, -- Insert emoji (default) or complete its name
+                  ---@type string|table|fun():table
+                  trigger = function()
+                    return { ":" }
+                  end,
+                },
+                -- should_show_items = function()
+                --   return vim.tbl_contains(
+                --     -- Enable emoji completion only for git commits and markdown.
+                --     -- By default, enabled for all file-types.
+                --     { "gitcommit", "markdown", "text" },
+                --     vim.o.filetype
+                --   )
+                -- end,
+              }
+            },
           },
 
           keymap = {
@@ -257,6 +280,7 @@ in {
     trouble-nvim
 
     blink-cmp
+    blink-emoji-nvim
   ]);
 
   packages = with pkgs; [
