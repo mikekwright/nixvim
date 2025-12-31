82585f5546867280b00897de560aa31a23e90773
Michael Wright <michael@level.co>
2025-12-31 13:50:45 -0700

    A few other useful keyboard shortcuts for working with terminals

--- common/keymaps.nix
+++ common/keymaps.nix
@@ -11,7 +11,9 @@ let
     tkeymap("<C-w>j", "<C-\\><C-n><C-w>j")
     tkeymap("<C-w>k", "<C-\\><C-n><C-w>k")
     tkeymap("<C-w>l", "<C-\\><C-n><C-w>l")
+    tkeymap("<C-w>n", "<C-\\><C-n>:bnext<CR>")
     tkeymap("<C-t>", "<C-\\><C-n>")
+    keymap("<C-w>n", ":bnext<CR>")
 
     -- This key opens a new terminal
     keymap("<C-t>", ":new<CR>:terminal<CR>i")  -- The extra i should put it in insert mode for the terminal
